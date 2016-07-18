fe9553b6d48df67f5ffdb9468021a9193c74ceed
Trent Whitinger <twhitinger@gmail.com>
2016-07-17 23:07:39 -0600

    da fuck

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,49 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :image_path)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :artists do 
+    resources :songs, only: [:new, :create]
+  end
+
+  resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
+end
